2247da5439e0fd8fe20f8ba5870070a89bac60c5
SaavedraMd <saavedracesar700@gmail.com>
2024-06-27 11:48:31 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
-describe('Array', function() {
-  it('should start empty', function() {
+describe('Array', () => {
+  it('should start empty', () => {
     const arr = [];
 
     expect(arr).to.be.an('array').that.is.empty;
