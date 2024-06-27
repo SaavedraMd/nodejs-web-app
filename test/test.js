2f0caec2f5c3cc94cb31be7b0111ad2342082fa5
SaavedraMd <saavedracesar700@gmail.com>
2024-06-27 09:39:31 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,11 @@
+// test/test.js
+const { expect } = require('chai');
+
+describe('Array', function() {
+  it('should start empty', function() {
+    const arr = [];
+
+    expect(arr).to.be.an('array').that.is.empty;
+  });
+});
 
